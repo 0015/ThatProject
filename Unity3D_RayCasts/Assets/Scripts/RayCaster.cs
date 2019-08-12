ef6796fd25bf233a8a9f899a06887a2625901aa6
Eric <luv.ilho@gmail.com>
2019-08-12 13:03:08 -0700

    Unity3D | Tutorial | Raycast/Boxcast/Spherecast

---
+++ Unity3D_RayCasts/Assets/Scripts/RayCaster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class RayCaster : MonoBehaviour
+{
+    void OnDrawGizmos()
+    {
+        float maxDistance = 10f;
+        RaycastHit hit;
+
+        bool isHit = Physics.Raycast(transform.position, transform.forward, out hit, maxDistance);
+        if (isHit)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, transform.forward * hit.distance);
+        }
+        else
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(transform.position, transform.forward * maxDistance);
+        }
+    }
+}
